c9f2a6762d569648e79f5dceb3f769be5f75416c
Yancale <idantalor100@gmail.com>
2018-09-13 13:35:45 -0700

    added the get data to a place and fixed the get initial placese bug.

--- google_places.py
+++ google_places.py
@@ -1,10 +1,11 @@
 from googleAPI import *
 from googlemaps import client 
 
-SEARCH_KEY_WORD = "best things to do"
-RADIUS          = 10000
-API_KEY         = "AIzaSyAtVGYjp7gGHRu0YcoRAxyB_IruztcMx1s"
-WANTED_VALUES   = (u'rating', u'name',u'geometry',u'photos')
+SEARCH_KEY_WORD        = "best things to do"
+RADIUS                 = 10000
+API_KEY                = "AIzaSyAtVGYjp7gGHRu0YcoRAxyB_IruztcMx1s"
+WANTED_VALUES          = (u'rating', u'name',u'geometry',u'photos')
+USER_SEARCH_INPUT_TYPE = "textquery" 
 
 class google_places(object):
     """
@@ -13,7 +14,15 @@ class google_places(object):
     def __init__(self):
         super(google_places, self).__init__()
         self.client = client.Client(key=API_KEY)
-        
+    """
+        filter the data in a dictionary.
+    """
+    def _get_desired_data(self, result):
+        output_dict = {}
+        for detail in WANTED_VALUES:
+            output_dict[detail] = result[detail]
+        return output_dict
+
     """
         this function get the initial places, give it a place and it returns a dict with the WANTED VALUES details. 
     """
@@ -23,12 +32,23 @@ class google_places(object):
         places_result = places(client = self.client, query = query_to_search , radius = RADIUS)
         results_list = places_result.get(u'results')
         for result in results_list:
-            output_dict = {}
-            try:
-                data_dict = places_result.get(u'results').pop()
-            except IndexError:
-                return "failed"
-            for detail in WANTED_VALUES:
-                output_dict[detail] = data_dict[detail] 
-            output_list.append(output_dict)
+            output = self._get_desired_data(result = result)
+            output_list.append(output)
         return output_list
+
+
+    """
+        get the data of a desired place.
+    """
+    def get_data_on_place(self, in_query):
+        output_list = []
+        find_place_result = find_place(client = self.client, input = in_query, input_type = USER_SEARCH_INPUT_TYPE)
+        places_result = place(client = self.client, place_id = find_place_result.get(u'candidates').pop().get(u'place_id'))
+        result = places_result.get(u'result')
+        if(result == None):
+            return output_list
+        output = self._get_desired_data(result = result)
+        output_list.append(output)
+        return output_list 
+
+

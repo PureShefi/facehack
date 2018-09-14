437d40dd02c84a6a06503bbf348aa770f829b511
Zohar <wwzohar@gmail.com>
2018-09-14 07:55:43 +0300

    wikiAPI: returns correct error message

--- wikiAPI.py
+++ wikiAPI.py
@@ -3,7 +3,7 @@ import wikipedia
 def get_wiki(params):
     try:
         return {"success": True, "data":wikipedia.summary(params["name"]), "url": wikipedia.page(params["name"]).url}
-    except wikipedia.exceptions.PageError:
-        return {"success": False}
+    except:
+        return {"success": False, "data": "", "url": ""}
 
     
